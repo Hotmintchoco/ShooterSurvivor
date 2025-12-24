60da98cd7afc6d259874686f2d37e53fc53fd7c2
Jeongmin Kim <95326550+Hotmintchoco@users.noreply.github.com>
2025-12-24 21:12:08 +0900

    피격시 경직 효과로 변경

--- Assets/1Scripts/Enemy.cs
+++ Assets/1Scripts/Enemy.cs
@@ -7,9 +7,9 @@ public class Enemy : LivingEntity
 {
     NavMeshAgent pathfinder;
     Transform target;
-    Rigidbody rigid;
     
     bool isChase;
+    bool isHit;
     float refreshRate = 0.25f;
 
     protected override void Start()
@@ -17,7 +17,6 @@ public class Enemy : LivingEntity
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        rigid = GetComponent<Rigidbody>();
 
         StartCoroutine(UpdatePath());
 
@@ -37,28 +36,36 @@ public class Enemy : LivingEntity
 
     public override void TakeHit(float damage, RaycastHit hit)
     {
-        StartCoroutine(KnockBack());
+        StartCoroutine(HitStop());
         base.TakeHit(damage, hit);
     }
-
+    
     IEnumerator UpdatePath()
     {
         while (target != null)
         {
             Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
             
-            if (isChase && !dead)
+            if (isChase && !dead && !isHit)
                 pathfinder.SetDestination(targetPosition);
             
             yield return new WaitForSeconds(refreshRate);
         }
     }
 
-    IEnumerator KnockBack()
+    IEnumerator HitStop()
     {
-        yield return null;
-        Vector3 playerPos = new Vector3(target.position.x, 0, target.position.z);
-        Vector3 dirVec = transform.position - playerPos;
-        rigid.AddForce(dirVec.normalized * 3, ForceMode.Impulse);
+        isHit = true;
+        pathfinder.isStopped = true; // 이동 멈춤
+        pathfinder.velocity = Vector3.zero; // 현재 관성으로 미끄러지는 것 방지
+
+        // 0.5초 정도 멈춰있게 설정 (원하는 시간만큼 조절)
+        yield return new WaitForSeconds(0.5f);
+
+        if (!dead) // 죽지 않았다면 다시 이동 재개
+        {
+            isHit = false;
+            pathfinder.isStopped = false; // 이동 다시 시작
+        }
     }
 }
